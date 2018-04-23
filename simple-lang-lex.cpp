5267be89d8b3a4fbf877c34214f67c4d1482642a
Ivan Garkavy <jimmycalling@yandex.ru>
2018-04-23 22:28:25 +0300

    now working, added some stuff

--- simple-lang-lex.cpp
+++ simple-lang-lex.cpp
@@ -18,10 +18,13 @@ bool isDigit(char c)
 
 template<class T>
 class Table {
-	//элементы нумеруются с первого (интерфейс)
+	//элементы нумеруются с 1 в интерфейсе
+	//в векторе нулевой элемент - "пустой"
+	//при инициализации таблицы этот пустой элемент указывать не нужно,
+	//	начинать сразу с первого.
 	vector<T> contents;
 public:
-	Table() = default;
+	Table(): contents{ T() } {}
 	Table(initializer_list<T> list)
 	{
 		contents.push_back( T() );
@@ -33,56 +36,69 @@ public:
 	//если такого элемента нет в таблице, он заносится в таблицу;
 	//в любом случае, возвращается номер элемента в таблице
 	{
-		auto res = std::find(contents.begin() + 1, contents.end(), elem);
+		cout << "PUT: " << elem << endl; //DEBUG
+		typename vector<T>::const_iterator res = 
+			std::find(contents.begin() + 1, contents.end(), elem);
+		int num = res - contents.begin();
+		//cout << "res == end(): " << (res == contents.end()) << endl; //DEBUG
+		//cout << "num = " << num << endl; //DEBUG
 		if(res == contents.end() ) {
 			contents.push_back(elem);
 		}
-		return res - contents.begin();
+		return num;
 	}
 	int find(const T& elem) const
 	//возвращает номер элемента в таблице;
 	//если не найден, то 0
 	{
-		auto res = std::find(contents.begin() + 1, contents.end(), elem);
-		return res != contents.end() ? res - contents.begin() : 0;
+		cout << "FIND: " << elem << endl; //DEBUG
+		typename vector<T>::const_iterator res = std::find(contents.begin() + 1, contents.end(), elem);
+		//cout << "res == " << (res - contents.begin()) << endl; //DEBUG
+		//cout << "res == end(): " << (res == contents.end()) << endl; //DEBUG
+		int num = res != contents.end() ? res - contents.begin() : 0;
+		//cout << "num = " << num << endl; //DEBUG
+		return num;
 	}
+	const T& operator[](int ind) const {
+		return contents[ind];
+	}	
 	vector<T> getVector() const {return contents;}
 };
 	
 Table<string> tw {
-	"program",
-	"int",
-	"string",
-	"real",
-	"not",
-	"and",
-	"or",
-	"if",
-	"else",
-	"do",
-	"while",
-	"break"
+	"program",	//1
+	"int",		//2
+	"string",	//3
+	"real",		//4
+	"not",		//5
+	"and",		//6
+	"or",		//7
+	"if",		//8
+	"else",		//9
+	"do",		//10
+	"while",	//11
+	"break"		//12
 };
 
 Table<string> td {
-	"{",
-	"}",
-	"(",
-	")",
-	",",
-	";",
-	"+",
-	"-",
-	"*",
-	"/",
-	"%",
-	"<",
-	">",
-	"<=",
-	">=",
-	"==",
-	"!=",
-	"="
+	"{",	//1
+	"}",	//2
+	"(",	//3
+	")",	//4
+	",",	//5
+	";",	//6
+	"+",	//7
+	"-",	//8
+	"*",	//9
+	"/",	//10
+	"%",	//11
+	"<",	//12
+	">",	//13
+	"<=",	//14
+	">=",	//15
+	"==",	//16
+	"!=",	//17
+	"="		//18
 };
 
 Table<string> tableIdent;
@@ -92,12 +108,13 @@ class Lex {
 public:
 	enum Type {
 		LEX_NULL = 0,
-		KEYWORD,
-		DIVISOR,
-		IDENT,
-		CONST_INT,
-		CONST_REAL,
-		CONST_STRING
+		
+		KEYWORD,		//1
+		DIVISOR,		//2
+		IDENT,			//3
+		CONST_INT,		//4
+		CONST_REAL,		//5
+		CONST_STRING	//6
 	};
 private:
 	Type type;
@@ -122,6 +139,7 @@ public:
 		int num;
 		switch(a_type) {
 			case KEYWORD:
+				cout << "***" << __LINE__ << endl; //DEBUG
 				num = tw.find(str);
 				if(num > 0) {
 					value = num;
@@ -130,6 +148,7 @@ public:
 				}
 				break;
 			case DIVISOR:
+				cout << "***" << __LINE__ << endl; //DEBUG
 				num = td.find(str);
 				if(num > 0) {
 					value = num;
@@ -147,6 +166,31 @@ public:
 				throw false;
 		}
 	}
+	friend ostream& operator<<(ostream& stream, Lex lexem) {
+		switch(lexem.type) {
+			case LEX_NULL:
+				break;
+			case KEYWORD:
+				stream << tw[lexem.value];
+				break;
+			case DIVISOR:
+				stream << td[lexem.value];
+				break;
+			case IDENT:
+				stream << tableIdent[lexem.value];
+				break;
+			case CONST_INT:
+				stream << lexem.value;
+				break;
+			case CONST_REAL:
+				//позже
+				break;
+			case CONST_STRING:
+				//позже
+				break;
+		}
+		return stream;
+	}
 	Type getType() { return type; }
 	int getValue() { return value; }
 };
@@ -154,7 +198,7 @@ public:
 class Scanner {
 	typedef bool (Scanner::*State)(char c);
 	istream& input;
-	State state = nullptr;
+	State state = &Scanner::stateInit;
 	bool lexIsRead = true;
 	//vector<Lex> lexemes;
 	string buf;
@@ -165,38 +209,39 @@ class Scanner {
 	bool stateFin(char c);
 public:
 	explicit Scanner(istream& a_input): input(a_input) {}
-	//bool check(const char* text); //V? string
 	bool readLex();
 	Lex getCurLex() const { return curLex; }
+	bool lexIsRead_() const { return lexIsRead; } //DEBUG
 };
-
-/*
-bool Scanner::check(const char* text)
-{
-	assert(text != nullptr);
-	state = &Scanner::stateInit;
-	for( ; *text != '\0'; text++) {
-		if( !(this->*state) (*text) ) {
-			return false;
-		}
-	}
-	return state == &Scanner::stateFin;
-}
-*/
 
 bool Scanner::readLex()
 {
+	cout << "***" << __LINE__ << endl; //DEBUG
+	if(!lexIsRead) {
+		cout << "***" << __LINE__ << endl; //DEBUG
+		return false;
+	}
 	char c;
 	lexIsRead = false;
-	bool ok = input.good();
+	bool ok = !lexIsRead;
 	bool test;
 	while(ok) {
+		cout << endl;
+		cout << "***" << __LINE__ << endl; //DEBUG
+		if(!input.good()) {
+			cout << "***" << __LINE__ << endl; //DEBUG
+			return false;
+		}
 		input.get(c);
+		cout << "***" << __LINE__ << endl; //DEBUG
 		test = (this->*state) (c);
+		cout << "***" << __LINE__ << endl; //DEBUG
 		if(!test) {
+			cout << "***" << __LINE__ << endl; //DEBUG
 			lexIsRead = false;
 			return false;
 		}
+		cout << "***" << __LINE__ << endl; //DEBUG
 		ok = !lexIsRead;
 	}
 }
@@ -204,7 +249,9 @@ bool Scanner::readLex()
 
 bool Scanner::stateInit(char c)
 {
+	cout << "***" << __LINE__ << ", " << c << endl; //DEBUG
 	if( isLetter(c) ) {
+		buf.push_back(c);
 		state = &Scanner::stateIdent;
 	} else if ( isDigit(c) ) {
 		buf.push_back(c);
@@ -212,38 +259,43 @@ bool Scanner::stateInit(char c)
 	} else switch(c) {
 		case ' ': 
 			break;
+		case '\n':
+			break;
 		case '+': 
-			curLex = { Lex::DIVISOR, {c} };
+			curLex = { Lex::DIVISOR, string{c} };
 			lexIsRead = true;
 			break;
 		case '-':
-			curLex = { Lex::DIVISOR, {c} };
+			curLex = { Lex::DIVISOR, string{c} };
 			lexIsRead = true;
 			break;
 		case '*':
-			curLex = { Lex::DIVISOR, {c} };
+			curLex = { Lex::DIVISOR, string{c} };
 			lexIsRead = true;
 			break;
 		case '/':
-			curLex = { Lex::DIVISOR, {c} };
+			curLex = { Lex::DIVISOR, string{c} };
 			lexIsRead = true;
 			break;
 		case '(':
-			curLex = { Lex::DIVISOR, {c} };
+			curLex = { Lex::DIVISOR, string{c} };
 			lexIsRead = true;
 			break;
 		case ')':
-			curLex = { Lex::DIVISOR, {c} };
+			curLex = { Lex::DIVISOR, string{c} };
 			lexIsRead = true;
 			break;
 		default:
+			cout << "***" << __LINE__ << endl; //DEBUG
 			return false;
 	}
+	cout << "***" << __LINE__ << endl; //DEBUG
 	return true;
 }
 
 bool Scanner::stateNum(char c)
 {
+	cout << "***" << __LINE__ << ", " << c << endl; //DEBUG
 	if( isDigit(c) ) {
 		buf.push_back(c);
 	} else {
@@ -255,29 +307,41 @@ bool Scanner::stateNum(char c)
 		buf.clear();
 		state = &Scanner::stateInit;
 	}
+	return true;
 }
 
 bool Scanner::stateIdent(char c)
 {
+	cout << "***" << __LINE__ << ", " << c << endl; //DEBUG
 	if( isDigit(c) || isLetter(c) ) {
 		buf.push_back(c);
 	} else {
 		input.unget();
-		//cout << "ungetted\n"; //DEBUG
 		curLex = {buf};
 		lexIsRead = true;
 		buf.clear();
 		state = &Scanner::stateInit;
 	}
+	return true;
 }
 
 int main() {
 	Scanner scanner(cin);
-	for(int i = 0; i < 10; i++) {
+	cout << "***" << __LINE__ << endl; //DEBUG
+	vector<Lex> lexemes;
+	while(1) {
+		cout << endl << endl;
+		cout << "***" << __LINE__ << endl; //DEBUG
 		scanner.readLex();
-		int type = scanner.getCurLex().getType();
-		int value = scanner.getCurLex().getValue();
-		cout << type << "," << value << endl;
+		cout << "***" << __LINE__ << endl; //DEBUG
+		if(!scanner.lexIsRead_()) {
+			break;
+		}
+		lexemes.push_back( scanner.getCurLex() );
+	}
+	for(auto& item : lexemes) {
+		//cout << item.getType() << "; " << item.getValue() << endl;
+		cout << item << endl;
 	}
 }
 
